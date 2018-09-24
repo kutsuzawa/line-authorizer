652f715265c28f2e356e617ce975947797763911
cappyzawa <m153304w@st.u-gakugei.ac.jp>
2018-09-24 09:11:57 +0900

    fix channel test

--- channel_token_test.go
+++ channel_token_test.go
@@ -11,41 +11,58 @@ import (
 
 func TestChannelToken_Publish(t *testing.T) {
 	t.Helper()
-	cases := []struct {
-		name      string
-		inputFile string
-		expect    string
-	}{
-		{name: "Successful publishing channel token", inputFile: "testdata/channel_auth_token.json", expect: "W1TeHCgfH2Liwa"},
-		{name: "Failed publishing channel token", inputFile: "testdata/channel_auth_token.json", expect: "W1TeHCgfH2Liwa"},
-	}
-
-	for _, c := range cases {
+	t.Run("Successful publishing channel token", func(t *testing.T) {
 		ts := httptest.NewServer(http.HandlerFunc(
 			func(w http.ResponseWriter, r *http.Request) {
 				if r.URL.Path != "/v2/oauth/accessToken" {
 					w.WriteHeader(http.StatusNotFound)
 					return
 				}
-				b, _ := ioutil.ReadFile(c.inputFile)
+				b, _ := ioutil.ReadFile("testdata/channel_auth_token.json")
 				w.Write(b)
 				return
 			},
 		))
+		defer ts.Close()
 
-		t.Run(c.name, func(t *testing.T) {
-			config := authorizer.Config{
-				APIAddress: ts.URL,
-			}
-			client := authorizer.NewClient(config)
-			token, err := client.PublishChannelToken()
-			if err != nil {
-				t.Errorf("err shoud not occur, but err: %v", err)
-			}
-			if *token != c.expect {
-				t.Errorf("token should be %s, but actual is %s", c.expect, *token)
-			}
-		})
-		ts.Close()
-	}
+		config := authorizer.Config{
+			APIAddress: ts.URL,
+		}
+		client := authorizer.NewClient(config)
+		token, err := client.PublishChannelToken()
+		if err != nil {
+			t.Errorf("err shoud not occur, but err: %v", err)
+		}
+		if *token != "W1TeHCgfH2Liwa" {
+			t.Errorf("token should be %s, but actual is %s", "W1TeHCgfH2Liwa", *token)
+		}
+	})
+
+	t.Run("Failed publishing channel token", func(t *testing.T) {
+		ts := httptest.NewServer(http.HandlerFunc(
+			func(w http.ResponseWriter, r *http.Request) {
+				if r.URL.Path != "/v2/oauth/accessToken" {
+					w.WriteHeader(http.StatusNotFound)
+					return
+				}
+				b, _ := ioutil.ReadFile("testdata/channel_auth_token_err.json")
+				w.WriteHeader(http.StatusBadRequest)
+				w.Write(b)
+				return
+			},
+		))
+		defer ts.Close()
+
+		config := authorizer.Config{
+			APIAddress: ts.URL,
+		}
+		client := authorizer.NewClient(config)
+		token, err := client.PublishChannelToken()
+		if token != nil {
+			t.Errorf("token should be nil, but actual is %v", token)
+		}
+		if err == nil {
+			t.Errorf("err shoud occur, but it does not")
+		}
+	})
 }
